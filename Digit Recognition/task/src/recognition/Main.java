f2eec988862306ea8f631981261200a9643cf684
Ilya Rogov <ilya.rogov.vladislav@gmail.com>
2019-12-29 11:39:57 +0300

    Trash test code

--- Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/task/src/recognition/Main.java
@@ -1,8 +1,51 @@
 package recognition;
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code herexsdsddgerqhr
+
+        int[] weights = {2, 1, 2, 4, -4, 4, 2, -1, 2, -5};
+        int[] input = new int[9];
+        String result = "";
+
+        System.out.println("Input grid: ");
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i <3 ; i++)
+            result += sc.nextLine();
+
+        char[] arrRes = result.toCharArray();
+
+        for (int i = 0; i <input.length; i++) {
+
+            if(arrRes[i] == '_')
+                input[i] = 0;
+            else
+                input[i] = 1;
+        }
+
+        if(new Main().result(weights, input) > 0 )
+            System.out.println("This number is 0" );
+        else
+            System.out.println("This number is 1");
+
+
+
     }
+
+    int result(int[] weight, int[] input){
+
+        int result = weight[weight.length - 1];
+
+        for (int i = 0; i < input.length; i++) {
+            result += weight[i] * input[i];
+        }
+
+        return result;
+
+    }
+
+
 }
