8a9f78b316c430cf4d47082f5cdd2f10692a6e79
Ilya <ilya.rogov.vladislav@gmail.com>
2020-01-09 01:35:14 +0300

    Add SerializationUtils class, console UI and state save.

--- Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/task/src/recognition/Main.java
@@ -1,37 +1,84 @@
 package recognition;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    static double[] inputs;
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         NetworkTools tools = new NetworkTools();
         Network net = new Network(15, 10);
         Teacher teacher = new Teacher(net, tools);
-        teacher.learn(5);
 
 
-        while(true){
-            System.out.println("Input grid");
-            inputs = takeInput();
+        double[] inputs;
+        int input;
+        int check = 1;
 
-            int maxIndex = 0;
-            double maxElem = net.calculate(inputs)[0];
-            for (int i = 1; i < net.calculate(inputs).length; i++) {
-                if (net.calculate(inputs)[i] > maxElem) {
-                    maxElem = net.calculate(inputs)[i];
-                    maxIndex = i;
-                }
+        while (check > 0) {
+            System.out.println("1. Learn the network\n" +
+                    "2. Guess a number");
+
+            switch (input = sc.nextInt()) {
+                case 1:
+                    System.out.println("Your choice: " + input);
+                    System.out.println("Learning...");
+                    teacher.learn(10);
+
+                    try {
+                        SerializationUtils.serialize(net,"network.out");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println("Done! Saved to the file.");
+                    break;
+
+                case 2:
+
+                    System.out.println("Your choice: " + input);
+                    try {
+                       net = (Network) SerializationUtils.deserialize("network.out");
+                    } catch (Exception e) {
+                        System.out.println("File not found ^(");
+                    }
+                    System.out.println("Input grid");
+                    inputs = takeInput();
+
+                    int maxIndex = 0;
+                    double maxElem = net.calculate(inputs)[0];
+                    for (int i = 1; i < net.calculate(inputs).length; i++) {
+                        if (net.calculate(inputs)[i] > maxElem) {
+                            maxElem = net.calculate(inputs)[i];
+                            maxIndex = i;
+                        }
+                    }
+
+                    System.out.println("It's number " + maxIndex);
+                    break;
+
+                default:
+                    check = 0;
+                    break;
             }
 
-            System.out.println("It's number " + maxIndex);
 
         }
+
+//        while(true){
+//            System.out.println("Input grid");
+//            inputs = takeInput();
+//
+//            int maxIndex = 0;
+//            double maxElem = net.calculate(inputs)[0];
+//            for (int i = 1; i < net.calculate(inputs).length; i++) {
+//                if (net.calculate(inputs)[i] > maxElem) {
+//                    maxElem = net.calculate(inputs)[i];
+//                    maxIndex = i;
+//                }
+
     }
 
     private static double[] takeInput() {
