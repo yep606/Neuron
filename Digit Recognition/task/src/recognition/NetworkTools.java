e7ba3e1319fa688add260c1cc631d1ae76d93bfd
Ilya Rogov <ilya.rogov.vladislav@gmail.com>
2020-01-08 20:03:07 +0300

    Finally completed self-learning.

--- Digit Recognition/task/src/recognition/NetworkTools.java
+++ Digit Recognition/task/src/recognition/NetworkTools.java
@@ -6,6 +6,7 @@ public class NetworkTools {
 
     private final double RATE_COEFFICIENT = 0.5;
     double[][][] mean;
+    double[][] middle = new double[10][15];
     Random random = new Random();
 
     public double[][] idealValues = new double[][]{{1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1}, //0
@@ -34,7 +35,7 @@ public class NetworkTools {
 
                 for (int prevNeuron = 0; prevNeuron < layerSizes[layer - 1]; prevNeuron++) {
 
-                    gaussNums[layer][neuron][prevNeuron] = random.nextGaussian();
+                    gaussNums[layer][neuron][prevNeuron] = 0;
 
                 }
             }
@@ -45,11 +46,14 @@ public class NetworkTools {
     public double[][] gaussBias(int... biases) {
 
         double[][] bias = new double[biases.length][biases[1]];
+        int inc = 0;
 
         for (int i = 0; i < biases.length; i++) {
             for (int j = 0; j < biases[i]; j++) {
-                bias[i][j] = random.nextGaussian();
+                bias[i][j] = 0;
+//                System.out.println(bias[i][j] + " - " + inc++);
             }
+            System.out.println();
         }
 
         return bias;
@@ -58,32 +62,27 @@ public class NetworkTools {
 
     public void deltaCalc(double[] output, int input) {
         double deltaW = 0;
-        mean = new double[output.length][15][10];
+        mean = new double[output.length][10][15];
 
-        for (int out = 0; out < output.length; out++) {
-            for (int cell = 0; cell < idealValues[input].length; cell++) {
+        for (int out = 0; out <output.length; out++) {
+            for (int cell = 0; cell <15; cell++) {
 
-                deltaW += RATE_COEFFICIENT * idealValues[input][cell] * (idealValues[out][cell] - output[out]);
+                deltaW = RATE_COEFFICIENT * idealValues[input][cell] * (idealValues[out][cell] - output[out]);
 
-                mean[out][cell][input] += deltaW;
+                mean[out][input][cell] = deltaW;
 
             }
         }
 
+        for (int out = 0; out < output.length ; out++) {
+            for (int weight = 0; weight < 15 ; weight++) {
+                for (int num = 0; num < 10; num++){
 
+                    middle[out][weight] += mean[out][num][weight] / 10;
 
-//        for (int out = 0; out < output.length; out++) { // 9
-//            for (int grid = 0; grid < 15; grid++) { //15
-//                for (int values = 0; values < 10; values++) { // 0..9
-//
-//                    deltaW += RATE_COEFFICIENT * idealValues[values][grid] * (idealValues[out][grid] - output[out]);
-//
-//                }
-//
-//                mean[out][grid] = deltaW / 10;
-//
-//            }
-//        }
+                }
+            }
+        }
 
     }
 
@@ -92,26 +91,14 @@ public class NetworkTools {
         for (int layer = 1; layer < netSize; layer++) {
             for (int neuron = 0; neuron < netLayerSize[layer]; neuron++) {
 
-                for (int prevNeuron = 0; prevNeuron < netLayerSize[layer - 1] ; prevNeuron++) {
+                for (int prevNeuron = 0; prevNeuron <netLayerSize[layer - 1] ; prevNeuron++) {
 
-                    weights[layer][netSize][prevNeuron] += (mean[neuron][prevNeuron][neuron] / 10);
+                    weights[layer][neuron][prevNeuron] += middle[neuron][prevNeuron];
 
                 }
 
             }
-
         }
-
-//        for (int layer = 1; layer < netSize; layer++) {
-//            for (int neuron = 0; neuron < netLayerSize[layer]; neuron++) {
-//
-//                for (int prevNeuron = 0; prevNeuron < netLayerSize[layer - 1]; prevNeuron++) {
-//                    weights[layer][neuron][prevNeuron] += mean[neuron];
-//                }
-//
-//            }
-//
-//        }
 
         return weights;
 
